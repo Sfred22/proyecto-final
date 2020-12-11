10d9559e6618220a582ee7b91adf42dd0e81717a
Sigifredo.Ramirez <sigi.16.ramiresco@gmail.com>
2020-12-11 09:40:09 -0600

    primer commit

---
+++ server/server.js
@@ -0,0 +1,46 @@
+require('./config/config')
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
+    next();
+});
+
+app.get('/', function(req, res) {
+    res.send('<h1>Bienvenido a mi servidor</h1>');
+});
+
+app.use(require('./routes/usuario'));
+app.use(require('./routes/categoria'));
+app.use(require('./routes/login'));
+app.use(require('./routes/productos'));
+
+
+// Conexión a la base de datos
+mongoose.connect('mongodb+srv://admin:0098765Ss@cluster0.wfzgv.mongodb.net/cafeteria', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('El servidor esta en linea por el puerto ', process.env.PORT);
+});
